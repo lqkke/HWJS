23af4bca2e5c5aa113f63b157fe6c7bd8b590c24
lqkke <fonizee@yandex.ru>
2024-08-16 19:17:19 +0300

    lesson02-02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,19 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6)
+let dice2 = Math.floor(Math.random() * 6)
+let isWinningDouble 
 
-/*
+if(dice1 === dice2) {
+  isWinningDouble = dice1
+}
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
+if (isWinningDouble > 3) {
   console.log('Выигрышный дубль!')
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
