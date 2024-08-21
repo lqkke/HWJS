e458eb39778d49e380bee5fc642dd55760322593
lqkke <fonizee@yandex.ru>
2024-08-22 00:45:42 +0300

    lesson04-04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -15,7 +15,7 @@
 function doubleEachCharacter(str) {
     let newString = ("");
     for (let i = 0; i < str.length; i++) {
-        newString += str[i], str[i];
+        newString += str[i] + str[i];
     }
 return newString; 
 }
