7ad1daec8bca8005b896f38442ddb842c7f56359
lqkke <fonizee@yandex.ru>
2024-09-11 12:11:33 +0300

    lesson09refactoring

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -27,7 +27,7 @@ console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 const map = (array, callback) => {
   const newArray = []
 
-  for (let i = 0; i < array.length) {
+  for (let i = 0; i < array.length; i++) {
     const element = array[i]
     const index = i
     
